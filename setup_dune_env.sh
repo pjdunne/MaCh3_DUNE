82c91c404b04f01da01f1a93c70c2970a7dbf687
EdAtkin <edhuthwaite@aol.com>
2022-11-04 11:45:00 +0000

    removing flags for specific builds of dependcies picked up from cvmfs.

--- setup_dune_env.sh
+++ setup_dune_env.sh
@@ -1,5 +1,5 @@
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
-setup cmake v3_19_6 -f Linux64bit+3.10-2.17
-setup root v6_18_02a -f Linux64bit+3.10-2.17 -q e17:prof
+setup cmake v3_19_6
+setup root v6_18_02a -q e17:prof
 export CXX=`which g++` # this might be specific for Fermilab?
 export CC=`which gcc` # this might be specific for Fermilab?
