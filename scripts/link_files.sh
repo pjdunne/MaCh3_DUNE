a17be892f7cb760bb2f2df2e2cb1060e6d29aaf1
Liban Warsame <ljw20@lxgpu04.hep.ph.ic.ac.uk>
2023-07-27 13:37:20 +0100

    Script changes + new flux xml + new exec in CMakeList

--- scripts/link_files.sh
+++ scripts/link_files.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 #
 # A script to link required files to the proper place i.e. where the sample config files will look for them
+# If running on IC machines this will work out of the box
+# If not change FILESDIR to point to where the files live
 
 MACH3DIR=`pwd`
-#FILESDIR=/vols/t2k/users/ljw20/data/DUNE_2021/DUNE_2021_splines_tdr_v8
-FILESDIR=/vols/dune/ea2817/DUNE_inputs/CAF_files_for_FD_TDR
+FILESDIR=/vols/dune/ljw20/
 
 if [ ! -d "$MACH3DIR/inputs/DUNE_CAF_files" ]
 then
   mkdir $MACH3DIR/inputs/DUNE_CAF_files
 fi
-ln -sf ${FILESDIR}/CAFs/*root inputs/DUNE_CAF_files
+ln -sf ${FILESDIR}/DUNE_2021_FD_CAFs/*root inputs/DUNE_CAF_files
 
 
 
@@ -18,5 +19,18 @@ if [ ! -d "$MACH3DIR/inputs/DUNE_spline_files" ]
 then
   mkdir $MACH3DIR/inputs/DUNE_spline_files
 fi
+ln -sf ${FILESDIR}/DUNE_2021_FD_splines/*root inputs/DUNE_spline_files
 
-ln -sf /vols/t2k/users/ljw20/data/DUNE_2021/DUNE_2021_splines/*root inputs/DUNE_spline_files
+if [ ! -d "$MACH3DIR/inputs/DUNE_ND_CAF_files" ]
+then
+  mkdir $MACH3DIR/inputs/DUNE_ND_CAF_files
+fi
+ln -sf ${FILESDIR}/DUNE_2023_ND_CAFs_FV_CCINC/*root inputs/DUNE_ND_CAF_files
+
+
+
+if [ ! -d "$MACH3DIR/inputs/DUNE_ND_spline_files" ]
+then
+  mkdir $MACH3DIR/inputs/DUNE_ND_spline_files
+fi
+ln -sf ${FILESDIR}/DUNE_2023_ND_splines_temp_new/*root inputs/DUNE_ND_spline_files
