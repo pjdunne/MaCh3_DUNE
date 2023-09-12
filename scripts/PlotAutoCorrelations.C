7845a46d6feb2aa67c56e6ba20850cdb0885f6c8
Liban Warsame <ljw20@lxgpu04.hep.ph.ic.ac.uk>
2023-09-12 11:37:02 +0100

    Plotting Scripts

---
+++ scripts/PlotAutoCorrelations.C
@@ -0,0 +1,49 @@
+#include <TString.h>
+#include <TFile.h>
+#include <TDirectory.h>
+#include <TCanvas.h>
+
+void PlotAutoCorrelations(std::string FileName) {
+
+  TFile* File = new TFile(FileName.c_str(),"READ");
+
+  TDirectory* AutoCorrDir2 = (TDirectory*)File->Get("Trace");
+  TIter next2(AutoCorrDir2->GetListOfKeys());
+  std::vector<TString> Titles;
+  TString HistName2;
+  TObject* obj2;
+  while (obj2 = next2()) {
+    HistName2 = obj2->GetName();
+	Titles.push_back(HistName2);
+  }
+
+  TDirectory* AutoCorrDir = (TDirectory*)File->Get("Auto_corr");
+  TIter next(AutoCorrDir->GetListOfKeys());
+
+  TString HistName;
+  TH1D* Hist;
+
+  TCanvas* Canvas = new TCanvas("Canv","");
+  Canvas->Print("AutoCorrND_x20.pdf[");
+
+  TObject* obj;
+  int i = 0;
+  while (obj = next()) {
+    HistName = obj->GetName();
+    std::cout << Titles[i] << std::endl;
+    Hist = (TH1D*)AutoCorrDir->Get(HistName);
+    //Hist->GetXaxis()->SetRangeUser(0, 2.7e6;
+    Hist->SetTitle(Titles[i]);
+    Hist->Draw();
+    Canvas->Print("AutoCorrND_x20.pdf");
+	i++;
+  }
+  Canvas->Print("AutoCorrND_x20.pdf]");
+  
+  /*
+  for (int i=0;i<1;i++) {
+    std::cout << KeyList[i] << std::endl;
+  }
+  */
+}
+
---
+++ scripts/PlotTraces.C
@@ -0,0 +1,40 @@
+#include <TString.h>
+#include <TFile.h>
+#include <TDirectory.h>
+#include <TCanvas.h>
+
+void PlotTraces(std::string FileName) {
+
+  TFile* File = new TFile(FileName.c_str(),"READ");
+  TDirectory* TraceDir = (TDirectory*)File->Get("Trace");
+  TIter next(TraceDir->GetListOfKeys());
+
+  TString HistName;
+  TH1D* Hist;
+
+  TCanvas* Canvas = new TCanvas("Canv","");
+  Canvas->Print("Traces_x20.pdf[");
+
+  TObject* obj;
+  while (obj = next()) {
+    HistName = obj->GetName();
+    std::cout << HistName << std::endl;
+	Hist = (TH1D*)TraceDir->Get(HistName);
+	Hist->SetBinContent(Hist->GetXaxis()->GetNbins(), Hist->GetBinContent(Hist->GetXaxis()->GetNbins()-1));
+	std::cout << "Min = " << Hist->GetMinimum() << std::endl;
+	std::cout << "Max = " << Hist->GetMaximum() << std::endl;
+	//Hist->GetYaxis()->SetRangeUser(0.95*Hist->GetMinimum(), 1.05*Hist->GetMaximum());
+
+    Hist->Draw();
+    //Hist->GetXaxis()->SetRangeUser(0, 2.7e6);
+    Canvas->Print("Traces_x20.pdf");
+  }
+  Canvas->Print("Traces_x20.pdf]");
+  
+  /*
+  for (int i=0;i<1;i++) {
+    std::cout << KeyList[i] << std::endl;
+  }
+  */
+}
+
