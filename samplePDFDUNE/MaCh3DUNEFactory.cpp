de84cded7e2067c2cb58d946edbdf2c1054a36d7
Daniel Barrow <danbarrow257@gmail.com>
2024-10-07 08:33:43 -0500

    Abstract samplePDF type behind config check in Factory so any samplePDF type can be initialised

--- samplePDFDUNE/MaCh3DUNEFactory.cpp
+++ samplePDFDUNE/MaCh3DUNEFactory.cpp
@@ -1,38 +1,40 @@
 #include "samplePDFDUNE/MaCh3DUNEFactory.h"
 
-void MakeMaCh3DuneBeamInstance(manager *FitManager, std::vector<samplePDFDUNEBeamFDBase*> &DUNEPdfs, covarianceXsec *&xsec, covarianceOsc *&osc){
+#include "samplePDFDUNE/samplePDFDUNEBeamFDBase.h"
+
+void MakeMaCh3DuneBeamInstance(manager *FitManager, std::vector<samplePDFFDBase*> &DUNEPdfs, covarianceXsec *&xsec, covarianceOsc *&osc){
 
   // there's a check inside the manager class that does this; left here for demonstrative purposes
   if (FitManager == nullptr) {
     std::cerr << "Didn't find a good config in input configuration" << std::endl;
     throw;
   }
-
+  
   //Check that you have specified some DUNE samples
   if(!FitManager->raw()["General"]["DUNESamples"]){
-	std::cerr << "You didn't specify any DUNESample configs to create samples from. Please add General:DUNESamples to your config" << std::endl;
-	throw;
+    std::cerr << "You didn't specify any DUNESample configs to create samples from. Please add General:DUNESamples to your config" << std::endl;
+    throw;
   }
-
+  
   //Check that you have specified some DUNE samples
   if(!FitManager->raw()["General"]["DUNESamplesPOT"]){
-	std::cerr << "You didn't specify the POT for each DUNE sample configs to create samples from. Please add General:DUNESamplesPOT to your config" << std::endl;
-	throw;
+    std::cerr << "You didn't specify the POT for each DUNE sample configs to create samples from. Please add General:DUNESamplesPOT to your config" << std::endl;
+    throw;
   }
- 
+  
   // Get inputted systematic parameters covariance matrices
   std::vector<std::string> xsecCovMatrixFile = FitManager->raw()["General"]["Systematics"]["XsecCovFile"].as<std::vector<std::string>>();
-
+  
   // Setup the covariance matrices
   if(xsec == nullptr){
-	xsec = new covarianceXsec(xsecCovMatrixFile, "xsec_cov");
+    xsec = new covarianceXsec(xsecCovMatrixFile, "xsec_cov");
   }
   else{
-	std::cout << "covariance Xsec has already been created so I am not re-initialising the object" << std::endl; 
+    std::cout << "covariance Xsec has already been created so I am not re-initialising the object" << std::endl; 
   }
   std::cout << "cov xsec setup" << std::endl;
   std::cout << "------------------------------" << std::endl;
-
+  
   std::string  OscMatrixFile = FitManager->raw()["General"]["Systematics"]["OscCovFile"].as<std::string>(); 
   std::string  OscMatrixName = FitManager->raw()["General"]["Systematics"]["OscCovName"].as<std::string>(); 
   std::vector<double> oscpars = FitManager->raw()["General"]["OscillationParameters"].as<std::vector<double>>();
@@ -41,16 +43,16 @@ void MakeMaCh3DuneBeamInstance(manager *FitManager, std::vector<samplePDFDUNEBea
     std::cout << oscpars[i] << " ";
   }
   std::cout << "}" << std::endl;
- 
+  
   osc = new covarianceOsc(OscMatrixName.c_str(), OscMatrixFile.c_str());
   osc->setName("osc_cov");
   std::cout << "Osc cov setup " << std::endl;
   std::cout << "------------------------------" << std::endl;
-
+  
   // ==========================================================
   //read flat prior, fixed paramas from the config file
   std::vector<std::string> XsecFixParams   = GetFromManager<std::vector<std::string>>(FitManager->raw()["General"]["Systematics"]["XsecFix"], {""});
-
+  
   // Fixed xsec parameters loop
   if (XsecFixParams.size() == 1 && XsecFixParams.at(0) == "All") {
     for (int j = 0; j < xsec->getSize(); j++) {
@@ -62,37 +64,51 @@ void MakeMaCh3DuneBeamInstance(manager *FitManager, std::vector<samplePDFDUNEBea
     }
   }
   std::cout << "xsec parameters loop done" << std::endl;
-
+  
   // Fill the parameter values with their nominal values
   // should _ALWAYS_ be done before overriding with fix or flat
   xsec->setParameters();
   osc->setParameters(oscpars); 
-
+  
   //####################################################################################
   //Create samplePDFDUNEBase Objs
   std::cout << "-------------------------------------------------------------------" << std::endl;
   std::cout << "Loading T2K samples.." << "\n" << std::endl;
   std::vector<std::string> DUNESampleConfigs = FitManager->raw()["General"]["DUNESamples"].as<std::vector<std::string>>();
   std::vector<double> DUNESamplePOTs = FitManager->raw()["General"]["DUNESamplesPOT"].as<std::vector<double>>();
-
+  
   if(DUNESampleConfigs.size() != DUNESamplePOTs.size()){
-	std::cerr << "Size of DUNESamples and DUNESamplesPOT is not the same and they need to be!" << std::endl;
-	throw;
+    std::cerr << "Size of DUNESamples and DUNESamplesPOT is not the same and they need to be!" << std::endl;
+    throw;
   }
-
+  
   for(unsigned int Sample_i = 0 ; Sample_i < DUNESampleConfigs.size() ; Sample_i++){
-	DUNEPdfs.push_back(new samplePDFDUNEBeamFDBase(DUNESamplePOTs[Sample_i], DUNESampleConfigs[Sample_i].c_str(), xsec));
-    DUNEPdfs[Sample_i]->UseNonDoubledAngles(true);
-    DUNEPdfs[Sample_i]->SetXsecCov(xsec);
-	DUNEPdfs[Sample_i]->SetOscCov(osc);
-	DUNEPdfs[Sample_i]->SetupOscCalc(osc->GetPathLength(), osc->GetDensity());
+
+    manager* tempSampleManager = new manager(DUNESampleConfigs[Sample_i].c_str());
+    std::string SampleType = tempSampleManager->raw()["SampleType"].as<std::string>();
+
+    samplePDFFDBase* FDBaseSample;
+    if (SampleType == "BeamFD") {
+      samplePDFDUNEBeamFDBase* BeamFDSample = new samplePDFDUNEBeamFDBase(DUNESamplePOTs[Sample_i], DUNESampleConfigs[Sample_i].c_str(), xsec);
+      FDBaseSample = (samplePDFFDBase*)BeamFDSample;
+    } else {
+      std::cerr << "Invalid SampleType:" << SampleType << " defined in config:" << DUNESampleConfigs[Sample_i] << std::endl;
+      throw;
+    }
+
+    FDBaseSample->UseNonDoubledAngles(true);
+    FDBaseSample->SetXsecCov(xsec);
+    FDBaseSample->SetOscCov(osc);
+    FDBaseSample->SetupOscCalc(osc->GetPathLength(), osc->GetDensity());
 
     // Pure for debugging, lets us set which weights we don't want via the manager
-    #if DEBUG_DUNE_WEIGHTS==1
-      DUNEPdfs[Sample_i]->setWeightSwitchOffVector(FitManager->getWeightSwitchOffVector());
-      DUNEPdfs[Sample_i]->setXsecWeightSwitchOffVector(FitManager->getXsecWeightSwitchOffVector());
-    #endif
-  }
+#if DEBUG_DUNE_WEIGHTS==1
+    FDBaseSample->setWeightSwitchOffVector(FitManager->getWeightSwitchOffVector());
+    FDBaseSample->setXsecWeightSwitchOffVector(FitManager->getXsecWeightSwitchOffVector());
+#endif
 
+    DUNEPdfs.push_back(FDBaseSample);
+  }
+  
   return;
 }
--- src/EventRatesDUNEBeam.cpp
+++ src/EventRatesDUNEBeam.cpp
@@ -13,105 +13,12 @@
 #include <TMath.h>
 
 #include "samplePDFDUNE/MaCh3DUNEFactory.h"
-
-
-std::string getNameNoExt(std::string name, std::string ext)  
-{                                                            
-  std::size_t pos ;                                          
-  pos = name.find(ext);                                      
-  name = name.substr(0,pos);                                 
-  return name ;                                              
-}                                                            
-                                                             
-void saveCanvas(TCanvas* canvas, std::string name, std::string legend)                                                                  
-{                                                            
-  name = getNameNoExt(name, ".root") ;                       
-  name = name + legend + ".root" ;                     
-  canvas -> SaveAs(name.c_str()) ;                           
-                                                             
-  name = getNameNoExt(name, ".root") ;                       
-  name = name + ".png" ;                                     
-  canvas -> SaveAs(name.c_str()) ;                           
-                                                             
-  name = getNameNoExt(name, ".png") ;                        
-  name = name + ".pdf" ;                                     
-  canvas -> SaveAs(name.c_str()) ;                           
-                                                             
-  name = getNameNoExt(name, ".pdf") ;                        
-  name = name + ".eps" ;                                     
-  canvas -> SaveAs(name.c_str()) ;                           
-                                                             
-} 
-
-void DrawModeBreakDown(samplePDFDUNEBeamFDBase*& Sample, std::unique_ptr<TFile> &File){
-
-  File->cd();
-  TH1D* ModeHistogram = nullptr;
-  THStack* StackedModes = new THStack(Sample->GetName().c_str(), "stack");
-  TLegend* leg = new TLegend(0.6, 0.7, 0.9, 0.9);
-
-  std::ofstream outfile;
-  std::string FileName=Sample->GetName()+"_EventRates.txt";
-  outfile.open(FileName);
-
-  outfile << "\\begin{table}[ht]" << std::endl;
-  outfile << "\\begin{center}" << std::endl;
-  outfile << "\\caption{Integral breakdown for sample: " << Sample->GetName() << "}" << std::endl;
-  outfile << "\\label{" << Sample->GetName() << "-EventRate}" << std::endl;
-
-
-  int space = 14;
-  TString nColumns;
-  nColumns += "|c|";
-  outfile << "\\begin{tabular}{|l" << nColumns.Data() << "}" << std::endl;
-  outfile << "\\hline" << std::endl;
-  outfile << "&" << std::setw(space) << Sample->GetName() << " " << std::endl;
-
-  double total = 0;
-  double mode_rate = 0;
-  for (unsigned int iMode=0;iMode<kMaCh3_nModes ;iMode++) {
-
-	////
-	std::string name = Sample->GetName();
-	name += MaCh3mode_ToDUNEString(static_cast<MaCh3_Mode>(iMode));
-	ModeHistogram = (TH1D*)Sample->get1DVarHist(kRecoNeutrinoEnergy, iMode);
-	ModeHistogram->SetName(name.c_str());
-	mode_rate = ModeHistogram->Integral();
-	total += mode_rate; 
-
-	outfile << MaCh3mode_ToDUNEString(static_cast<MaCh3_Mode>(iMode)).c_str() << std::setw(space) << " & " << mode_rate << " \\\\" << std::endl;
-
-	//Add this to the stacked histogram
-	if(ModeHistogram->Integral() > 0){
-	  ModeHistogram->SetFillColor(kRed+iMode);
-	  ModeHistogram->SetLineColor(kRed+iMode);
-	  StackedModes->Add(ModeHistogram);	
-	  leg->AddEntry(ModeHistogram, MaCh3mode_ToDUNEString(static_cast<MaCh3_Mode>(iMode)).c_str(), "f");
-	  ModeHistogram->Write();
-	}
-  }
-  outfile << "\\hline " << std::endl;
-  outfile << "&" << std::setw(space) << "Total:" << total << "\\\\ \\hline" << std::endl;
-
-  std::string PdfName = Sample->GetName();
-  PdfName += "_ModeStack.pdf";
-  StackedModes->Write();
-  TCanvas* c1 = new TCanvas("c1", "c1", 800, 600);
-  c1->cd();
-  StackedModes->Draw("HIST");
-  leg->Draw("SAMES");
-  c1->Print(PdfName.c_str());
-  delete c1;
-  
-
-  return;
-}
+#include "samplePDFDUNE/samplePDFDUNEBeamFDBase.h"
 
 int main(int argc, char * argv[]) {
-
   // ----------------------- OPTIONS ---------------------------------------- //
   if(argc == 1){
-    std::cout << "Usage: bin/mini_MCMC config.cfg" << std::endl;
+    std::cout << "Usage: bin/EventRatesDUNEBeam config.cfg" << std::endl;
     return 1;
   }
 
@@ -123,8 +30,8 @@ int main(int argc, char * argv[]) {
 
   //####################################################################################
   //Create samplePDFSKBase Objs
-  std::cout << "Loading T2K samples.." << "\n" << std::endl;
-  std::vector<samplePDFDUNEBeamFDBase*> DUNEPdfs;
+  std::cout << "Loading DUNE samples.." << "\n" << std::endl;
+  std::vector<samplePDFFDBase*> DUNEPdfs;
   MakeMaCh3DuneBeamInstance(fitMan, DUNEPdfs, xsec, osc); 
   //Setup the cross-section parameters
   //This should get the prior values.
@@ -142,23 +49,15 @@ int main(int argc, char * argv[]) {
   OutputFile->cd();
 
   for (unsigned sample_i = 0 ; sample_i < DUNEPdfs.size() ; ++sample_i) {
-    	
-	std::string name = DUNEPdfs[sample_i]->GetName();
-	sample_names.push_back(name);
-	TString NameTString = TString(name.c_str());
-
-	DUNEPdfs[sample_i] -> SetupOscCalc(osc->GetPathLength(), osc->GetDensity());
-	osc->setParameters();
-	DUNEPdfs[sample_i] -> reweight();
-	TH1D *SampleHistogram = (TH1D*)DUNEPdfs[sample_i] -> get1DHist() -> Clone(NameTString+"_unosc");
-	unoscillated_hists.push_back(SampleHistogram);
-
-	TCanvas *nomcanv = new TCanvas("nomcanv","",1200,600);
-	SampleHistogram -> Draw("HIST");
-
-	std::string plotname = "Dummy_Hist";
-	saveCanvas(nomcanv, plotname,"_nominal_spectra");
-	//DrawModeBreakDown(DUNEPdfs[sample_i], OutputFile);
+    std::string name = DUNEPdfs[sample_i]->GetName();
+    sample_names.push_back(name);
+    TString NameTString = TString(name.c_str());
+    
+    DUNEPdfs[sample_i] -> SetupOscCalc(osc->GetPathLength(), osc->GetDensity());
+    osc->setParameters();
+    DUNEPdfs[sample_i] -> reweight();
+    TH1D *SampleHistogram = (TH1D*)DUNEPdfs[sample_i] -> get1DHist() -> Clone(NameTString+"_unosc");
+    unoscillated_hists.push_back(SampleHistogram);
   }
  
   //Now print out some event rates, we'll make a nice latex table at some point 
