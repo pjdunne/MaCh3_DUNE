d86d000fa8cf12db9c7aab4e07d6ef7d42cdc5bb
Daniel Barrow <danbarrow257@gmail.com>
2024-11-12 05:57:59 -0600

    Update setup script to pull more recent ROOT install

--- setup_dune_env.sh
+++ setup_dune_env.sh
@@ -1,6 +1,6 @@
 source /cvmfs/larsoft.opensciencegrid.org/spack-packages/setup-env.sh
-spack load root@6.28.06
-spack load cmake
+spack load root@6.28.12
 spack load gcc@12.2.0
+
 export CXX=`which g++` # this might be specific for Fermilab?
 export CC=`which gcc` # this might be specific for Fermilab?
