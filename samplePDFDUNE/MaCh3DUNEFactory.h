b93a5765dd95071ccbc29485f0403bd6ef2c5326
Henry Wallace <henryi@narval3.narval.calcul.quebec>
2025-01-24 08:36:14 -0500

    Adds basic doxycomments

--- samplePDFDUNE/MaCh3DUNEFactory.h
+++ samplePDFDUNE/MaCh3DUNEFactory.h
@@ -6,5 +6,16 @@
 // Include the samplePDFs
 #include "samplePDF/samplePDFFDBase.h"
 
+/// @brief Factory function that generates MaCh3 DUNE instance including configured samples
+/// @param fitMan Configuration Manager 
+/// @param sample_vec Vector of samplePDF objects
+/// @param xsec Cross-section covariance matrix
+/// @param osc Oscillation covariance matrix
 void MakeMaCh3DuneInstance(manager *fitMan, std::vector<samplePDFFDBase*> &sample_vec,  covarianceXsec *&xsec, covarianceOsc *&osc);
+
+/// @brief Gets MaCh3 DUNE samplePDF instance
+/// @param SampleType Sample type (BeamFD, BeamND, Atm)
+/// @param SampleConfig Configuration file 
+/// @param xsec Cross-section covariance matrix
+/// @return samplePDF instance
 samplePDFFDBase* GetMaCh3DuneInstance(std::string SampleType, std::string SampleConfig, covarianceXsec* &xsec);
--- splines/splinesDUNE.h
+++ splines/splinesDUNE.h
@@ -3,14 +3,38 @@
 
 #include "splines/splineFDBase.h"
 
+/// @brief Specialisation of FD (binned) spline class
 class splinesDUNE : virtual public splineFDBase
 {
   public:
+	/// @brief Constructor
+	/// @param xsec_cov cross-section matrix
 	splinesDUNE(covarianceXsec* xsec_cov);
+
+	/// @brief Destructor
 	virtual ~splinesDUNE();
-	void SetupSplines(int BinningOpt);
+
+	// HW: Doesn't appear to be implemented in DUNE splines, commented for now...
+	// void SetupSplines(int BinningOpt);
+
+	/// @brief Fills indexing for each sample and generates a large spline vector
+	/// @param SampleName Name of sample
+	/// @param OscChanFileNames names of oscillation channels in the sample
 	virtual void FillSampleArray(std::string SampleName, std::vector<std::string> OscChanFileNames) override;
+
+	/// @brief Find sand strips dubplicate modes to ensure everything corresponds to MaCh3 modes
+	/// @param InputVector Input vector of smodes
+	/// @return Processed vector with merged modes
 	virtual std::vector< std::vector<int> > StripDuplicatedModes(std::vector< std::vector<int> > InputVector) override;
+
+	/// @brief Getter method for each spline
+	/// @param SampleName Name of sample
+	/// @param iOscChan Oscillation channel of sample
+	/// @param EventMode Mode of event
+	/// @param Var1Val Bin 1 value
+	/// @param Var2Val Bin 2 value
+	/// @param Var3Val Bin 3 value
+	/// @return Value of spline for given bin
 	virtual std::vector< std::vector<int> > GetEventSplines(std::string SampleName, int iOscChan, int EventMode, double Var1Val, double Var2Val, double Var3Val) override;
 };
 
