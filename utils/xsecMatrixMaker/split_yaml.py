09d96c48d8bb33685b9375cc0e5d84d9a0cb8762
Hank Hua <h.hua23@imperial.ac.uk>
2025-02-17 17:50:00 +0000

    Moved tdr_cov_grouped.yaml; moved split_yaml.py and added cli to specify input file and output folder; edited xsec cov file specified in EventRates_Beam.yaml and changed global step scale to match TDR value.

--- utils/xsecMatrixMaker/split_yaml.py
+++ utils/xsecMatrixMaker/split_yaml.py
@@ -1,6 +1,7 @@
 import yaml
+import argparse
 
-def get_from_full_yaml(out_name,
+def get_from_full_yaml(input_file, out_name,
                        det_id=None,
                        syst_type=None,
                        horn_current=None,
@@ -15,7 +16,7 @@ def get_from_full_yaml(out_name,
         horn_current = ['fhc','rhc']
     if param_groups is None:
         param_groups = ['Flux','Xsec','DetSys']
-    with open("tdr_covs_grouped.yaml", 'r') as stream:
+    with open(input_file, 'r') as stream:
         try:
             f = yaml.safe_load(stream)
         except yaml.YAMLError as exc:
@@ -59,6 +60,14 @@ def get_from_full_yaml(out_name,
             yaml.dump(new_yaml, stream)
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Split full yaml into smaller yaml files')
+    parser.add_argument('input', type=str, help='Input yaml file')
+    parser.add_argument('output_folder', type=str, help='Output folder')
+    
+    args = parser.parse_args()
+    input_file = args.input
+    output_folder = args.output_folder
+    
     det_ids = [1,24,25]
     syst_types = ['Norm','Spline','Functional']
     param_groups = ['Flux','Xsec','DetSys']
@@ -67,24 +76,24 @@ if __name__ == '__main__':
     for det_id in det_ids:
         det_id_name = 'ND' if det_id == 1 else ('FD' if det_id == 24 else 'ND+FD')
         for param_group in param_groups:
-            get_from_full_yaml(f"tdr_covs/{param_group}_{det_id_name}.yaml",
+            get_from_full_yaml(input_file, f"{output_folder}/{param_group}_{det_id_name}.yaml",
                                 det_id=[det_id],
                                 syst_type=syst_types,
                                 horn_current=horn_currents,
                                 param_groups=[param_group])
-            get_from_full_yaml(f"tdr_covs/{param_group}_{det_id_name}_uniform.yaml",
+            get_from_full_yaml(input_file, f"{output_folder}/{param_group}_{det_id_name}_uniform.yaml",
                                 det_id=[det_id],
                                 syst_type=syst_types,
                                 horn_current=horn_currents,
                                 param_groups=[param_group],
                                 uniform_stepscale=True)
 
-    get_from_full_yaml('tdr_covs/Flux_ND+FD.yaml',
+    get_from_full_yaml(input_file, f'{output_folder}/Flux_ND+FD.yaml',
                           det_id=[1,24],
                           syst_type=syst_types,
                           horn_current=['fhc','rhc'],
                           param_groups=['Flux'])
-    get_from_full_yaml('tdr_covs/Flux_ND+FD_uniform.yaml',
+    get_from_full_yaml(input_file, f'{output_folder}/Flux_ND+FD_uniform.yaml',
                        det_id=[1,24],
                           syst_type=syst_types,
                             horn_current=['fhc','rhc'],
