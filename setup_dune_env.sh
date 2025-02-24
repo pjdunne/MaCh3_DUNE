6b443bc7fe33b611137fd1126bdb29ad281a3ae6
Daniel Barrow <daniel.barrow@physics.ox.ac.uk>
2025-02-24 17:53:05 +0000

    Update setup_dune_env.sh script

--- setup_dune_env.sh
+++ setup_dune_env.sh
@@ -2,3 +2,5 @@ source /cvmfs/larsoft.opensciencegrid.org/spack-packages/setup-env.sh
 spack load root@6.28.12
 spack load cmake
 spack load gcc@12.2.0
+
+source /cvmfs/larsoft.opensciencegrid.org/spack-packages/opt/spack/linux-almalinux9-x86_64_v2/gcc-12.2.0/root-6.28.12-sfwfmqorvxttrxgfrfhoq5kplou2pddd/bin/thisroot.sh
